72debde1ddce7d43a908995a45291e1577f0ae43
Leandro <leanangelico@gmail.com>
2022-12-13 15:57:39 -0300

    css

--- sonar/src/components/NavBar/NavBar.jsx
+++ sonar/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,25 @@
 import Container from 'react-bootstrap/Container';
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, NavLink } from 'react-router-dom';
 import CardWidget from '../CartWidget/CartWidget';
-import '../NavBar/navbar.css'
+import './styles.css'
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
     <Container fluid>
-    <NavLink className={ ( {isActive} )=> isActive ? 'btn btn-success' : 'btn btn-outline-success'} to='/node_modules'>Sonar</NavLink>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          style={{ maxHeight: '100px' }}
-          navbarScroll
-        >
+    <NavbarBrand className='logo'>Sonar</NavbarBrand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse>
+        <Nav className="me-auto my-2 my-lg-0">
+          <NavLink 
+               className={ ( {isActive} )=> isActive ? 'btn btn-success' : 'btn btn-outline-success'} 
+                to="/node_modules"
+                >
+                         Inicio
+          </NavLink>
           <NavLink 
                className={ ( {isActive} )=> isActive ? 'btn btn-success' : 'btn btn-outline-success'} 
                 to="/categoria/gorra"
